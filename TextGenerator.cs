089a8b729ed4aa97245547e5214be4d506d600eb
giuliom95 <giuliomartella1995@gmail.com>
2020-05-24 02:10:58 +0200

    First working version

---
+++ TextGenerator.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class TextGenerator : MonoBehaviour
+{
+    public Sprite[] Fontface;
+    public string Text;
+    public SpriteRenderer TextCharacter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GenerateText()
+    {
+        if(Text != null) {
+            int column = 0;
+            foreach(char ch in Text) {
+                if(Char.IsDigit(ch)) {
+                    GenerateChar(Fontface[ch - '0'], column);
+                } else if(char.IsLetter(ch)) {
+                    char chu = char.ToUpper(ch);
+                    GenerateChar(Fontface[chu - 'A' + 10], column);
+                } else if(ch == '.' || ch == ',') {
+                    GenerateChar(Fontface[36], column);
+                } else if(ch == ':') {
+                    GenerateChar(Fontface[37], column);
+                }
+                ++column;
+            }
+        }
+    }
+
+    void GenerateChar(Sprite sprite, int column) {
+        GameObject o = Instantiate(TextCharacter.gameObject, transform);
+        o.transform.localPosition = new Vector3(column*10, 0, 0);
+        o.GetComponent<SpriteRenderer>().sprite = sprite;
+    }
+}
